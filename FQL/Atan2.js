a0f1bb7bbdcc7c646757396107bd70019f458939
Taro <potatopotaro@taros-mbp.home>
2020-05-12 18:35:38 -0500

    fixed Atan2

--- FQL/Atan2.js
+++ FQL/Atan2.js
@@ -2,19 +2,19 @@ const Atan2 = (q) => (y, x) =>
   q.Let(
     { pi: 3.141592653589793 },
     q.If(
-      q.GT(q.Var("x"), 0),
-      q.Atan(q.Divide(q.Var("y"), q.Var("x"))),
+      q.GT(x, 0),
+      q.Atan(q.Divide(y, x)),
       q.If(
-        q.And(q.LT(q.Var("x"), 0), q.GTE(q.Var("y"), 0)),
-        q.Add(q.Atan(q.Divide(q.Var("y"), q.Var("x"))), q.Var("pi")),
+        q.And(q.LT(x, 0), q.GTE(y, 0)),
+        q.Add(q.Atan(q.Divide(y, x)), q.Var("pi")),
         q.If(
-          q.And(q.LT(q.Var("x"), 0), q.LT(q.Var("y"), 0)),
-          q.Subtract(q.Atan(q.Divide(q.Var("y"), q.Var("x"))), q.Var("pi")),
+          q.And(q.LT(x, 0), q.LT(y, 0)),
+          q.Subtract(q.Atan(q.Divide(y, x)), q.Var("pi")),
           q.If(
-            q.And(q.Equals(q.Var("x"), 0), q.GT(q.Var("y"), 0)),
+            q.And(q.Equals(x, 0), q.GT(y, 0)),
             q.Divide(q.Var("pi"), 2),
             q.If(
-              q.And(q.Equals(q.Var("x"), 0), q.LT(q.Var("y"), 0)),
+              q.And(q.Equals(x, 0), q.LT(y, 0)),
               q.Subtract(0, q.Divide(q.Var("pi"), 2)),
               0
             )
